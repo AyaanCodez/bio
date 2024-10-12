36d0e8bf366c14d05fe66076c82f64dfc02c0882
Jone <haseebdigit@gmail.com>
2024-10-12 09:36:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: ['#ff007f', '#0096FF'] // Pink and Blue
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 7, // Bubbles
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: false // Disable lines
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: false, // No movement on hover
+        mode: 'grab'
+      },
+      onclick: {
+        enable: false, // No repulse on click
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 200,
+        line_linked: {
+          opacity: 0
+        }
+      },
+      bubble: {
+        distance: 200,
+        size: 15,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+});
